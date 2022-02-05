aaa038bce5bb721fa5fc7171e7ed84b1e9439db4
Daniel Ho <dho1994@gmail.com>
2022-02-04 20:50:05 -0800

    Update covers for 2 more books

--- client/components/constants.js
+++ client/components/constants.js
@@ -5,9 +5,9 @@ const EXCLUDED_BOOKS = {
   'https://blueocean.s3.us-west-1.amazonaws.com/Immersive 3D Design Visualization With Autodesk Maya and Unreal Engine 4 by Abhishek Kumar (z-lib.org).epub': true,
   'https://blueocean.s3.us-west-1.amazonaws.com/Immersive 3D Design Visualization With Autodesk Maya and Unreal Engine 4.epub': true,
   'https://blueocean.s3.us-west-1.amazonaws.com/Immersive+3D+Design+Visualization+With+Autodesk+Maya+and+Unreal+Engine+4.epub': true,
-  'https://blueocean.s3.us-west-1.amazonaws.com/Cracking the Coding Interview - 189 Programming Questions and Solutions (6th Edition).epub': true,
   'https://blueocean.s3.us-west-1.amazonaws.com/Bids, Tenders and Proposals Winning Business Through Best Practice by Harold Lewis (z-lib.org).pdf.epub': true,
-  'https://blueocean.s3.us-west-1.amazonaws.com/A Beginner’s Guide to 3D Modeling.epub': true,
+  'https://blueocean.s3.us-west-1.amazonaws.com/Bids%2C%20Tenders%20and%20Proposals%20by%20Harold%20Lewis.epub': true,
+  'https://blueocean.s3.us-west-1.amazonaws.com/Cracking the Coding Interview - 189 Programming Questions and Solutions (6th Edition).epub': true,
   'https://blueocean.s3.us-west-1.amazonaws.com/Peace State by Amber Bird.epub': true,
   'https://blueocean.s3.us-west-1.amazonaws.com/Dead Sound by Anise Eden.epub': true,
 };
@@ -21,6 +21,8 @@ const EDITED_TITLES = {
 
 const EDITED_COVERS = {
   'https://blueocean.s3.us-west-1.amazonaws.com/Submitting a Winning Bid.epub': 'Submitting a Winning Bid.jpeg',
+  'https://blueocean.s3.us-west-1.amazonaws.com/Bids, Tenders and Proposals by Harold Lewis.epub': 'Bids, Tenders and Proposals by Harold Lewis.jpg',
+  'https://blueocean.s3.us-west-1.amazonaws.com/A Beginner’s Guide to 3D Modeling.epub': 'A Beginner’s Guide to 3D Modeling.jpg',
 };
 
 export default { EXCLUDED_BOOKS, EDITED_TITLES, EDITED_COVERS };
